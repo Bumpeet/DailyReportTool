81d52aa7c3c32a6ea1610ec74e7bc2fc97c8361a
Bumpeet <pavanchetan81@gmail.com>
2023-04-06 17:49:27 +0530

    succesfully ran the code with caliburnMicro for the first time

---
+++ Models/DealerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport.Models
+{
+    public class DealerModel
+    {
+        public string DealerName { get; set; }
+        public string LastName { get; set; }
+    }
+}

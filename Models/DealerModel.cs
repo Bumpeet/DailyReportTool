331424292f3c62a662a69253b4b09b637c2d5c48
Bumpeet <pavanchetan81@gmail.com>
2023-04-06 19:01:47 +0530

    added the database connection and also retrieving the distributor names part

--- Models/DealerModel.cs
+++ Models/DealerModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DailyReport.Models
 {
@@ -10,5 +12,54 @@ namespace DailyReport.Models
     {
         public string DealerName { get; set; }
         public string LastName { get; set; }
+    }
+
+    public class DataConnect
+    {
+        public string conString;
+        public SqlConnection con;
+        public DataConnect()
+        {
+            conString = "Data Source=ATI-9\\MSSQLSERVER1;Initial Catalog=Rough;Persist Security Info=True;User ID=sa;Password=_056pwjr";
+
+            con = new SqlConnection(conString);
+
+        }
+
+        public DataTable FetchDistributorData()
+        {
+            DataTable data = new DataTable();
+            using (con)
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand("select * from Distributors"))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        data.Load(reader);
+
+                    }
+                }
+
+            }
+            return data;
+        }
+
+        public List<string> FetchDistributorName()
+        {
+            List<string> data = new List<string>();
+            //DataRow[] names;
+
+            DataTable wholeData = this.FetchDistributorData();
+            //names = wholeData.Select("DistributorName");
+
+            //return names;
+            foreach (DataRow row in wholeData.Rows)
+            {
+                data.Add($"row['DistributorName']");
+            }
+            return data;
+            
+        }
     }
 }
--- ViewModels/Page1ViewModel.cs
+++ ViewModels/Page1ViewModel.cs
@@ -4,10 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using DailyReport.Models;
+using System.Data;
 
 namespace DailyReport.ViewModels
 {
+
     public class Page1ViewModel: Screen
     {
+        DataConnect data = new DataConnect();
+        List<string> selectedDistributor;
+
+        //public List<string> _selectedDistributor;
+        //public List<string> SelectedDistributor
+        //{
+        //    set
+        //    {
+        //        _selectedDistributor = value;
+        //    }
+        //    get
+        //    {
+
+        //        return _selectedDistributor;
+        //    }
+        //}
+
+        public Page1ViewModel()
+        {
+            selectedDistributor = data.FetchDistributorName();
+        }
+
+
+
+        
+
+        
+
+
     }
+
 }
